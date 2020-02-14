faba121d191058f4cf04859f289e723dab35d7bf
Yakoo <yakoo_steam@163.com>
2020-02-14 15:46:08 +0800

    save github repo address for source code pro font

--- install.sh
+++ install.sh
@@ -6,10 +6,11 @@ vimrtdir=~/.vim_runtime
 if [ ! -d ${vimrtdir} ]
 then
     test -e ~/.vimrc && mv ~/.vimrc ~/.vimrc_bak
-    git clone https://github.com/amix/vimrc.git ${vimrtdir} 
+    git clone https://github.com/amix/vimrc.git ${vimrtdir}
     git clone https://github.com/vim-scripts/taglist.vim ${vimrtdir}/sources_non_forked/vim-taglist
     git clone https://github.com/vim-scripts/wombat256.vim ${vimrtdir}/sources_non_forked/vim-wombat256
     git clone https://github.com/Raimondi/delimitMate.git ${vimrtdir}/sources_non_forked/delimitMate
+    # git clone https://github.com/adobe-fonts/source-code-pro.git
 fi
 
 sh ${vimrtdir}/install_awesome_vimrc.sh
